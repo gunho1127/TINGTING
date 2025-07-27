a4098a0425d04554521a18751a541857b7c328ed
gunho11127 <rjsghsky@gmail.com>
2025-07-27 12:16:53 +0900

    로그인, 회원가입 기능 완료

--- src/main/java/com/TingTing/controller/SignController.java
+++ src/main/java/com/TingTing/controller/SignController.java
@@ -1,8 +1,14 @@
 package com.TingTing.controller;
 
+import com.TingTing.dto.SignInRequest;
+import com.TingTing.dto.SignUpRequest;
+import com.TingTing.dto.TokenResponse;
+import com.TingTing.entity.User;
 import com.TingTing.service.SignService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,7 +25,15 @@ public class SignController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponse> signin(@RequestBody SignInRequest dto) {
-        return ResponseEntity.ok(authService.logIn(dto));
+    public ResponseEntity<TokenResponse> signin(@RequestBody SignInRequest dto,
+                                                HttpServletResponse response) {
+        return ResponseEntity.ok(authService.logIn(dto, response));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@AuthenticationPrincipal User user,
+                                       HttpServletResponse response) {
+        authService.logOut(user, response);
+        return ResponseEntity.ok().build();
     }
 }
