2e058b436e84974404191036684ca38ec11cdee3
gunho11127 <rjsghsky@gmail.com>
2025-08-18 13:30:13 +0900

    WS 허용 오리진 수정

--- src/main/java/com/TingTing/config/WebSocketConfig.java
+++ src/main/java/com/TingTing/config/WebSocketConfig.java
@@ -12,7 +12,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws/chat")
+                .setAllowedOrigins("http://localhost:3000")
+                .withSockJS();
     }
 
     @Override
